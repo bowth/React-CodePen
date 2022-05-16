b4ccb67282acaea54fa56a3e17606a7e37861fa5
nawshi <nawshadhnawas@gmail.com>
2022-05-16 13:03:54 +0300

    new

---
+++ build/precache-manifest.300cd6ef851b23d19bc57aefb2e52642.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cab220e054be6d0631801b857c1de2d2",
+    "url": "/Online-Code-Editor.git/index.html"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/Online-Code-Editor.git/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "9fa333bc02a56fdcd01c",
+    "url": "/Online-Code-Editor.git/static/css/main.7d2118c4.chunk.css"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/Online-Code-Editor.git/static/js/2.4254f81d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Online-Code-Editor.git/static/js/2.4254f81d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fa333bc02a56fdcd01c",
+    "url": "/Online-Code-Editor.git/static/js/main.c467d7d3.chunk.js"
+  },
+  {
+    "revision": "b3ad33bac0a50e40a7d6",
+    "url": "/Online-Code-Editor.git/static/js/runtime-main.44e8705e.js"
+  }
+]);
